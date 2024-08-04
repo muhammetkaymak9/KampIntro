940d8fed95c7a075dd21e70f8230853a7928dd3e
muhammetkaymak9 <m9kaymak@gmail.com>
2024-08-04 09:48:32 +0300

    commit four

---
+++ OOP2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "122345";
+            musteri1.TcNo = "222222222";
+            musteri1.Adi = "Muhammet";
+            musteri1.Soyadi = "Kaymak";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 1;
+            musteri2.MusteriNo = "122345";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "12324345672413";
+
+            Musteri musteri3 =  new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager musteriManager = new CustomerManager();
+
+        }
+    }
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager)
+        {
+            //başvuran bilgilerini değerlendirme
+            //
+
+            krediManager.Hesapla();
+        }
+    }
+}
